517bee1f2066957bc1f35e47f4680f17891e6516
O-w-it <omar.abdu20187@gmail.com>
2026-03-29 16:29:09 +0300

    تعديل ملف الاختبار

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:smart_store/main.dart';
+import 'package:smart_store/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
