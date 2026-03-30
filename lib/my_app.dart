a7059e6c6b33737a804f4f9ec4e33863cb63e07c
Ali Mutahhar Ali <alftyhyly35@gmail.com>
2026-03-30 16:34:23 -0700

    تفعيل الالترجمه للعريه والانجليزيه في التطبيق

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'views/screens/main_wrapper_screen.dart';
-
+import 'package:easy_localization/easy_localization.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: MainWrapperScreen(), // الشاشة التي تحتوي على الـ BottomNav
     );
   }
