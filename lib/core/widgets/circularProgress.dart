8b92b92ef160d576b28c9fd08d56a7c046a7ab88
O-w-it <omar.abdu20187@gmail.com>
2026-03-31 22:45:00 +0300

    اضافة ملف CircularProgress

---
+++ lib/core/widgets/circularProgress.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../constants/app_colors.dart';
+
+class CircularProgress extends StatelessWidget {
+  final double size;
+  const CircularProgress({
+    super.key,
+    this.size = 50.0,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SpinKitDualRing(
+        color: AppColors.primary,
+        size: size,
+        lineWidth: 3.0, // لجعل الدائرة نحيفة وأنيقة مثل سناب
+      ),
+    );
+  }
+}
