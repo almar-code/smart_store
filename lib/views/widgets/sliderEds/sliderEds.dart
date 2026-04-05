33964e348010729eb34764a293a9a695188b2563
O-w-it <omar.abdu20187@gmail.com>
2026-04-05 17:02:24 +0300

    وميض الاعلانات

--- lib/views/widgets/sliderEds/sliderEds.dart
+++ lib/views/widgets/sliderEds/sliderEds.dart
@@ -1,8 +1,18 @@
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-class SliderEds extends StatelessWidget {
-  SliderEds({super.key});
+
+import '../flash/flash_screen.dart'; // تأكد من إضافة المكتبة
+
+class SliderEds extends StatefulWidget {
+  const SliderEds({super.key});
+
+  @override
+  State<SliderEds> createState() => _SliderEdsState();
+}
+
+class _SliderEdsState extends State<SliderEds> {
+  bool _isLoading = true; // حالة التحميل
+
   final List<String> imgList = [
     'assets/images/E3.jpg',
     'assets/images/a4.jpg',
@@ -10,14 +20,29 @@ class SliderEds extends StatelessWidget {
     'assets/images/E2.jpg',
     'assets/images/a4.jpg',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    // مؤقت لمدة 5 ثوانٍ للوميض
+    Future.delayed(const Duration(seconds: 4), () {
+      if (mounted) setState(() => _isLoading = false);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
+    // إذا كان في حالة تحميل، نعرض مربعاً يلمع بنفس أبعاد السلايدر
+    if (_isLoading) {
+      return Flasheds();
+    }
+
+    // الكود الأصلي الخاص بك يظهر بعد 5 ثوانٍ
+    return SizedBox(
       child: CarouselSlider(
         options: CarouselOptions(
           height: 200,
           autoPlay: true,
-          autoPlayInterval: Duration(seconds: 6),
+          autoPlayInterval: const Duration(seconds: 3),
           enlargeCenterPage: true,
           enlargeFactor: .15,
           enlargeStrategy: CenterPageEnlargeStrategy.scale,
