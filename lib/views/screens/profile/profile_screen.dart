915fe4b122b23be85128c8fe9d0e17afd7359286
Ali Mutahhar Ali <alftyhyly35@gmail.com>
2026-04-05 18:56:22 -0700

    تعديل عرض بيانات المستخدم من داخل الابار الي البدي

--- lib/views/screens/profile/profile_screen.dart
+++ lib/views/screens/profile/profile_screen.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-
-import '../../../core/widgets/icons/app_icon.dart';
 import '../../../core/widgets/user_profile.dart';
-import '../home/home_screen.dart';
+import '../../widgets/profile/profile_list.dart';
+import 'package:easy_localization/easy_localization.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
   @override
-  Widget build(BuildContext context) =>  Scaffold(
+  Widget build(BuildContext context) {
+    context.locale;
+    return Scaffold(
     appBar: AppBar(
-        toolbarHeight: 90, // تحديد نفس ارتفاع الودجيت
-        titleSpacing: 0,
-     title:  UserProfileOrLogin(
-       avatarRadius:20,
-       isLoggedIn: true,
-       userName: "Ali Mutahar",
-       userImageUrl: "assets/images/Gemini_Generated_Image_ez61caez61caez61.png", // أو اتركها null للأيقونة
-       onLoginTap: () {
-       },
-       onProfileTap: () {
-       },
-     )
 
     ),
-      body: Center(child: Text("Profile Page")
-      ));
+      body: Column(
+
+        children: [
+          SizedBox(
+            height: 90,
+            child: UserProfileOrLogin(
+              avatarRadius:20,
+              isLoggedIn: true,
+              userName: "Ali Mutahar",
+              userImageUrl: "assets/images/Gemini_Generated_Image_ez61caez61caez61.png", // أو اتركها null للأيقونة
+              onLoginTap: () {
+              },
+              onProfileTap: () {
+              },
+            ),
+          ),
+          Expanded(child: ProfileList()),
+        ],
+      ),
+      );
+  }
 }
