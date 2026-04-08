cf27c530cf6b58cb2514fa0683e30248537a32f8
O-w-it <omar.abdu20187@gmail.com>
2026-04-08 23:17:25 +0300

    تصميم ابار صفحة المفضلة

--- lib/views/screens/favorites/favorites_screen.dart
+++ lib/views/screens/favorites/favorites_screen.dart
@@ -1,8 +1,37 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/app_colors.dart';
+import '../../../core/widgets/app_title.dart';
+import '../../../core/widgets/icons/app_icon.dart';
+import '../../../core/widgets/icons/arrow_back_icon.dart';
+
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
+  final bool screenOnly;
+  const FavoritesScreen({super.key,this.screenOnly =false});
   @override
-  Widget build(BuildContext context) => const Scaffold(
-      body: Center(child: Text("Favorites Page")));
+  Widget build(BuildContext context) {
+    bool isDesktop = MediaQuery.of(context).size.width > 800;
+    return Scaffold(
+        backgroundColor: AppColors.background,
+        appBar: AppBar(
+            backgroundColor: AppColors.background,
+            elevation: 0,
+            scrolledUnderElevation: 0,
+            leading:Icon(
+              CupertinoIcons.heart,
+              color:  AppColors.iconColor,
+              size: isDesktop?22: 20,
+            ),
+            titleSpacing:isDesktop?0: 0,
+            title: AppTitle(firstPart: tr('firstFavorites'),secondPart: tr('secondFavorites'),fontSize: isDesktop?18: 15,spacing: ' ',),
+            actions: [
+              AppIcon(icon:CupertinoIcons.delete,color: AppColors.iconColor,size: 15,),
+              screenOnly ? ArrowBack() : SizedBox(),
+
+          ],
+        ),
+        body: Center(child: Text("Favorites Page")));
+}
 }
