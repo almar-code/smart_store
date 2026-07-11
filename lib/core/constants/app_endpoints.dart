dc1f5f3f7b971adcbbbb72cabf5ae85ce9e1f130
O-w-it <omar.abdu20187@gmail.com>
2026-07-11 21:24:35 +0300

    تعديل ملف الروابط

--- lib/core/constants/app_endpoints.dart
+++ lib/core/constants/app_endpoints.dart
@@ -19,7 +19,19 @@ class ApiEndpoints {
   static String get getSubCategories => '$_baseUrl/api/categories';
   static String get getProducts => '$_baseUrl/api/products';
   static String get getCategories => '$_baseUrl/api/sections';
+  // ====================  روابط المفضلات الجديدة ====================
+  // static String get getFavoriteProducts => '$_baseUrl/api/customer/favorites';
+  static String get toggleFavorite => '$_baseUrl/api/customer/favorites/toggle';
+  static String get favoriteCount => '$_baseUrl/api/customer/favorites/count';
 // ====================🛍 روابط ملفات صور  المنتجات والأقسام الثابتة ====================
   static String productImageUrl(String? image) => "$_baseUrl/storage/uploads/products/$image";
   static String subCategoryImageUrl(String? image) => "$_baseUrl/storage/uploads/subcategory/$image";
+  // لعرض محتويات السلة (GET) -> ستحتاج لتمرير customer_id كـ Query Parameter
+  static String get getCart => '$_baseUrl/api/cart';
+  // لإضافة منتج جديد إلى السلة (POST)
+  static String get addToCart => '$_baseUrl/api/cart/add';
+  // لتحديث كمية منتج في السلة (PUT) -> تعتمد على معرف السجل في السلة id
+  static String updateCartItem(int id) => '$_baseUrl/api/cart/update/$id';
+  // لحذف صنف من السلة (DELETE) -> تعتمد على معرف السجل في السلة id
+  static String deleteCartItem(int id) => '$_baseUrl/api/cart/delete/$id';
 }
